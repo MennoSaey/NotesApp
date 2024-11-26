9773b0840baba363d57d8a387089e98d49c75b7b
MennoSeay <mennosaey@hotmail.com>
2024-11-26 12:14:59 +0100

    Initial project setup with MAUI

---
+++ NotesApp.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NotesApp.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NotesApp.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NotesApp.Mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
